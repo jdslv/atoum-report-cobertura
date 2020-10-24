b302c2d8cca31fe40a18be17d2f3cf75195d3b70
Joel <jdasilva@corp.free.fr>
2020-10-24 16:07:13 +0200

    Add coverage

--- .atoum.php
+++ .atoum.php
@@ -4,8 +4,6 @@ function fullPath(string ...$parts): string {
     return __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts);
 }
 
-$script->addDefaultReport();
-
 $runner->addTestsFromDirectory(fullPath('tests', 'units'));
 
 $runner
@@ -13,3 +11,41 @@ $runner
         ->setWatchedFiles([fullPath('src')])
 ;
 $runner->addExtension(new mageekguy\atoum\xml\extension($script));
+
+
+# reports
+if (extension_loaded('xdebug') === true) {
+    $script->enableBranchAndPathCoverage();
+    $script->noCodeCoverageInDirectories(fullPath('vendor'));
+
+    if (!getenv('CI')) {
+        // Show default report
+        $script->addDefaultReport();
+
+        $path = fullPath('reports', 'coverage');
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        // HTML report
+        $coverage = new mageekguy\atoum\reports\coverage\html();
+        $coverage
+            ->addWriter(new mageekguy\atoum\writers\std\out())
+            ->setOutPutDirectory($path)
+        ;
+        $runner->addReport($coverage);
+    }
+
+    # coverage report
+    $covFile = fullPath('reports', 'coverage.xml');
+    $path = pathinfo($covFile, PATHINFO_DIRNAME);
+
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+
+    $cobertura = new mageekguy\atoum\reports\cobertura();
+    $cobertura->addWriter(new mageekguy\atoum\writers\file($covFile));
+    $runner->addReport($cobertura);
+}

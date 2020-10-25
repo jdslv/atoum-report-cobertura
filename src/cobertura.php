ff510fe789bdbd9e3b9b466cbde9ec5fcd6e2693
Joel <jdasilva@corp.free.fr>
2020-10-25 03:16:16 +0100

    Create report

--- src/cobertura.php
+++ src/cobertura.php
@@ -3,7 +3,72 @@
 namespace mageekguy\atoum\reports;
 
 use mageekguy\atoum;
+use mageekguy\atoum\reports\cobertura\reflection;
+use mageekguy\atoum\reports\cobertura\score;
+use mageekguy\atoum\reports\cobertura\xml;
 
 class cobertura extends atoum\reports\coverage
 {
+    protected $score = null;
+
+    public function build($event): self
+    {
+        if ($event !== atoum\runner::runStop) {
+            return $this;
+        }
+
+        $document = new xml\document;
+        $document->addSource($this->adapter->getcwd());
+
+        $files = $this->score->getClasses();
+        $methods = $this->score->getMethods() ?? [];
+        $branches = $this->score->getBranches() ?? [];
+        $paths = $this->score->getPaths() ?? [];
+        $classes = $this->uniqueKeys($methods, $branches, $paths);
+
+        foreach ($classes as $className) {
+            if (!array_key_exists($className, $methods)) {
+                $methods[$className] = [];
+            }
+
+            if (!array_key_exists($className, $branches)) {
+                $branches[$className] = [];
+            }
+
+            if (!array_key_exists($className, $paths)) {
+                $paths[$className] = [];
+            }
+
+            $classMethods = $this->uniqueKeys($methods[$className], $branches[$className], $paths[$className]);
+            $reflectedClass = new reflection\klass($className);
+            $namespace = $reflectedClass->getNamespaceName();
+
+            foreach ($classMethods as $methodName) {
+                $reflectedMethod = new reflection\method($className, $methodName);
+
+                $methodCoverage = new score\coverage;
+                $methodCoverage->branchesAreAvailable($branches[$className][$methodName] ?? []);
+                $methodCoverage->linesAreAvailable($methods[$className][$methodName] ?? []);
+                $methodCoverage->pathsAreAvailable($paths[$className][$methodName] ?? []);
+
+                $document->addMethod($reflectedMethod, $methodCoverage);
+            }
+        }
+
+        $this->string = $document->toXML();
+
+        return $this;
+    }
+
+    public function handleEvent($event, atoum\observable $observable)
+    {
+        $this->score = ($event !== atoum\runner::runStop ? null : $observable->getScore());
+
+        return parent::handleEvent($event, $observable);
+    }
+
+    public function uniqueKeys(array ...$data): array
+    {
+        return array_unique(array_keys(array_merge(...$data)));
+    }
 }
